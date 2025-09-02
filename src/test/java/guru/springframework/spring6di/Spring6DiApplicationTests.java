9c9f8e5f0a849bab8d68e7b7962e6827442ffc29
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-09-02 14:12:39 +0200

    test

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,31 @@
 package guru.springframework.spring6di;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.spring6di.controllers.MyController;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+    @Autowired
+    ApplicationContext applicationContext;
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testGetControllerFromCtx() {
+        MyController controller = applicationContext.getBean(MyController.class);
+        System.out.println("In test from context");
+        System.out.println(controller.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromInstance() {
+        System.out.println(myController.sayHello());
+        System.out.println("In test from instance ");
+    }
 
     @Test
     void contextLoads() {
